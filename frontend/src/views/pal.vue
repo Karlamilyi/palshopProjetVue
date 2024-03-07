3bcece9076cf20ef1daa83266cb75871e76d1f69
Zongotripledozo <113369286+Zongotripledozo@users.noreply.github.com>
2024-03-07 23:22:58 +0100

    detail pal par id

--- frontend/src/views/pal.vue
+++ frontend/src/views/pal.vue
@@ -34,13 +34,15 @@
       };
     },
     mounted() {
-      axios.get('http://127.0.0.1:5000/pals') // Faites attention à l'URL de l'API
-        .then(response => {
-          this.pal = response.data[Object.keys(response.data)[0]]; // Récupérer le premier élément des données
-        })
-        .catch(error => {
-          console.error('Une erreur s\'est produite lors de la récupération des données du Pal', error);
-        });
+        const palId = this.$route.params.id;
+        axios.get('http://127.0.0.1:5000/pals')
+  .then(response => {
+    const key = palId; 
+    this.pal = response.data[key];
+  })
+  .catch(error => {
+    console.error('Une erreur s\'est produite lors de la récupération des données du Pal', error);
+  });
     },
   };
   </script>
@@ -51,7 +53,7 @@
   padding: 0;
   background-color: #FFEDE1;
 }
-  /* Votre style existant */
+  
   .pal-page {
     min-height: 100%;
     display: flex;
