bb350628f5c3758aefc698272e1d18bcd0d42832
Beuhnnyto <adam.malek@efrei.net>
2024-03-07 16:42:39 +0100

    going back to api in nodeJS

--- backend/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-routes_blueprint = Blueprint("routes", __name__)
-
-from .catalogRoutes import catalogRoutes
-from .homeRoutes import homeRoutes
-
-routes_blueprint.register_blueprint(catalogRoutes)
-routes_blueprint.register_blueprint(homeRoutes)
