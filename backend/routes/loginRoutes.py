42db3743d7e8decdbe879297c217b15890433426
Beuhnnyto <adam.malek@efrei.net>
2024-03-07 20:56:50 +0100

    Add email import to loginRoutes.py

--- backend/routes/loginRoutes.py
+++ backend/routes/loginRoutes.py
@@ -1,3 +1,4 @@
+import email
 from flask import Blueprint, jsonify, request
 from flask_login import login_user, login_required, logout_user
 from config import db,Login
@@ -7,15 +8,4 @@ loginRoutes = Blueprint("loginRoutes", __name__)
 @loginRoutes.route("/register", methods=["POST"])
 def register():
     data = request.get_json()
-    email = data["email"]
-    password = data["password"]
-
-    user = Login.query.filter_by(email=email).first()
-    if user:
-        return jsonify({"message": "User Already Exists"})
-
-    new_user = Login(email=email)  # Create a new user object
-    new_user.set_password(password)  # Set password securely (e.g., using passlib)
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({"message": "User Created Successfully"})
+    print(data)
