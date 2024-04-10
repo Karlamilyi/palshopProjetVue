8570da66a20e30cb13b9f205e8ed0dfc4b6ccfda
Beuhnnyto <adam.malek@efrei.net>
2024-04-10 19:24:08 +0200

    Update MongoDB URI in config.py

--- backend/config.py
+++ backend/config.py
@@ -2,11 +2,9 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
 
-uri = "mongodb+srv://Beuhnnyto:obcMfdkwnb4FVLph@beuhnnyto.tvogdgo.mongodb.net/?retryWrites=true&w=majority&appName=Beuhnnyto"
+uri = "mongodb+srv://Beuhnnyto:ARISestUNhumainWallah@beuhnnyto.tvogdgo.mongodb.net/?retryWrites=true&w=majority&appName=Beuhnnyto"
 client = MongoClient(uri, server_api=ServerApi("1"))
 db = client.Palshop
 Pals = db.paldex
 Login = db.accounts
 Contacts = db.contacts
-
-
