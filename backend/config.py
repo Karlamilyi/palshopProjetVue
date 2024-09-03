1dd89983c8ae1d66178d99de6370cfd194f30fa8
Beuhnnyto <117911391+Beuhnnyto@users.noreply.github.com>
2024-09-03 20:35:15 +0200

    security breach

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,7 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
-
-uri = "mongodb+srv://Beuhnnyto:ARISestUNhumainWallah@beuhnnyto.tvogdgo.mongodb.net/?retryWrites=true&w=majority&appName=Beuhnnyto"
+#connection string uri
 client = MongoClient(uri, server_api=ServerApi("1"))
 db = client.Palshop
 Pals = db.paldex
